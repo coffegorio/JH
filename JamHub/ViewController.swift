977572ad2dba3b027489fb4f4e60421b7746f4f7
coffegorio <coffegorio@vk.com>
2025-02-07 02:14:13 +0300

    Initial Commit

---
+++ JamHub/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JamHub
+//
+//  Created by Егорио on 07.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
