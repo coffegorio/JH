8414718f9eb4a0f24a19b6a9a0045b01cdd75be5
coffegorio <coffegorio@vk.com>
2025-02-09 04:08:17 +0300

    .

--- JamHub/Screens/ExperienceOfPlaying/View/ExperienceOfPlayingVC.swift
+++ JamHub/Screens/ExperienceOfPlaying/View/ExperienceOfPlayingVC.swift
@@ -11,6 +11,18 @@ import SnapKit
 class ExperienceOfPlayingVC: UIViewController {
     
     private let titleLabel = CustomLabel(text: "Привет! Был ли у тебя опыт игры на гитаре?", textAlignment: .left, fontWeight: .bold)
+    private let neverButton = CustomButton(text: "Я никогда не играл", fontSize: 16, fontWeight: .regular)
+    private let beginnerButton = CustomButton(text: "Я начал играть недавно", fontSize: 16, fontWeight: .regular)
+    private let intermediateButton = CustomButton(text: "Я играю на неплохом уровне", fontSize: 16, fontWeight: .regular)
+    private let advancedButton = CustomButton(text: "Я - профи", fontSize: 16, fontWeight: .regular)
+    
+    private lazy var buttonsStackView: UIStackView = {
+        let buttonsView = UIStackView(arrangedSubviews: [neverButton, beginnerButton, intermediateButton, advancedButton])
+        buttonsView.axis = .vertical
+        buttonsView.spacing = 20
+        buttonsView.alignment = .center
+        return buttonsView
+    }()
     
     
     override func viewDidLoad() {
@@ -28,6 +40,7 @@ class ExperienceOfPlayingVC: UIViewController {
         
         [
             titleLabel,
+            buttonsStackView
             
         ].forEach { view.addSubview($0) }
         
@@ -37,6 +50,11 @@ class ExperienceOfPlayingVC: UIViewController {
         
         titleLabel.snp.makeConstraints { make in
             make.top.equalToSuperview().offset(100)
+            make.leading.trailing.equalToSuperview().inset(40)
+        }
+        
+        buttonsStackView.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(50)
             make.leading.trailing.equalToSuperview().inset(40)
         }
         
